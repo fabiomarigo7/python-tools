3e5bab4f46baf1c0d459fc2a73c9d7354037a9db
Fabio Marigo <39345385+fabiomarigo7@users.noreply.github.com>
2019-03-29 13:22:23 +0100

    Update image_resizer_1024.py

--- image_resizer_1024.py
+++ image_resizer_1024.py
@@ -6,7 +6,7 @@ def resizeImage(infile, output_dir=""):
     outfile = os.path.splitext(infile)[0]+"_resized"
     extension = os.path.splitext(infile)[1]
 
-    if (extension == ".jpg"):
+    if ((extension == ".jpg") or (extension == ".jpeg") or (extension == ".JPG") or (extension == ".JPEG")):
 
         if infile != outfile:
             try :
