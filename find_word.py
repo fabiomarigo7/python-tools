1a8a6254ee5e2d527f38ccc7f4b6141f3624b40a
Fabio Marigo <39345385+fabiomarigo7@users.noreply.github.com>
2019-04-10 17:20:49 +0200

    Update find_word.py

--- find_word.py
+++ find_word.py
@@ -1,24 +1,32 @@
 import os
-print('=== find word ===\n\n')
-s = ''
-while (True):
-    n = 0
-    list = os.listdir()
-    print('-------------------------------------')
-    print("@ Write the string you are looking for")
-    print("@ Or write Q to quit\n")
-    s = input().lower()
-    print('')
-    if (s == 'q' or s=='Q'):
-        break;
-    elif (s.strip() == ''): 
-        print('@ Please write a valid string!\n')
-        pass
-    else:
-        for el in list:
-            if (el != "@ find word.py"):
-                if s in open(el).read().lower() or s in el.lower():
-                    print(el)
-                    n += 1
-        print('\n@ The operation has been done successfully!')
-        print('@ '+str(n)+' files contain the string.\n')
+
+def findWord():
+    s = ''
+    while (True):
+        n = 0
+        list = os.listdir()
+        
+        print('-------------------------------------')
+        print("@ Write the string you are looking for")
+        print("@ Or write Q to quit\n")
+        s = input().lower()
+        print('')
+        
+        if (s == 'q' or s=='Q'):
+            break;
+        
+        elif (s.strip() == ''): 
+            print('@ Please write a valid string!\n')
+            pass
+        else:
+            for el in list:
+                if (el != "@ find word.py"):
+                    if s in open(el).read().lower() or s in el.lower():
+                        print(el)
+                        n += 1
+            print('\n@ The operation has been done successfully!')
+            print('@ '+str(n)+' files contain the string.\n')
+        
+if __name__=="__main__":
+    print('=== find word ===\n\n')
+    findWord()
